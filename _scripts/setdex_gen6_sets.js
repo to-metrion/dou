3578c5db3d31dcfc537491782e2b1174003bba4a
to-metrion <20890062+to-metrion@users.noreply.github.com>
2021-10-16 01:46:09 -0400

    Adding XY DOU sets

--- _scripts/setdex_gen6_sets.js
+++ _scripts/setdex_gen6_sets.js
@@ -1,4 +1,50 @@
 var SETDEX_GEN6_SETS = {
+	"Aegislash": {
+		"Wide Guard Tank": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 4,
+				"sp": 0
+			},
+			"ivs": {
+				"sp": 0
+			},
+			"nature": "Quiet",
+			"ability": "Stance Change",
+			"item": "Weakness Policy",
+			"moves": [
+				"Shadow Ball",
+				"Flash Cannon",
+				"Wide Guard",
+				"King's Shield"
+			],
+			},
+		"Substitute Leftovers": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 4,
+				"sp": 0
+			},
+			"ivs": {
+				"sp": 0
+			},
+			"nature": "Quiet",
+			"ability": "Stance Change",
+			"item": "Leftovers",
+			"moves": [
+				"Shadow Ball",
+				"Flash Cannon",
+				"Substitute",
+				"King's Shield"
+			],
+			}
+			},
 	"Amoonguss": {
 		"Rage Powder Sample": {
 			"evs": {
@@ -67,6 +113,257 @@ var SETDEX_GEN6_SETS = {
 			],
 			}
 			},
+	"Azumarill": {
+		"Belly Drum": {
+			"evs": {
+				"hp": 252,
+				"at": 252,
+				"df": 4,
+				"sa": 0,
+				"sd": 0,
+				"sp": 0
+			},
+			"nature": "Adamant",
+			"ability": "Huge Power",
+			"item": "Sitrus Berry",
+			"moves": [
+				"Aqua Jet",
+				"Knock Off",
+				"Play Rough",
+				"Belly Drum"
+			],
+			}
+			},
+	"Bisharp": {
+		"All-Out Attacker": {
+			"evs": {
+				"hp": 4,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Adamant",
+			"ability": "Defiant",
+			"item": "Life Orb",
+			"moves": [
+				"Knock Off",
+				"Sucker Punch",
+				"Iron Head",
+				"Protect"
+			],
+			},
+		"Memoric Slow Attacker": {
+			"evs": {
+				"hp": 252,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 4,
+				"sp": 0
+			},
+			"ivs": {
+				"sp": 0
+			},
+			"nature": "Adamant",
+			"ability": "Defiant",
+			"item": "Life Orb",
+			"moves": [
+				"Knock Off",
+				"Sucker Punch",
+				"Iron Head",
+				"Protect"
+			],
+			}
+			},
+	"Charizard": {
+		"Sun Attacker (Mega Y)": {
+			"evs": {
+				"hp": 248,
+				"at": 0,
+				"df": 64,
+				"sa": 88,
+				"sd": 0,
+				"sp": 108
+			},
+			"nature": "Modest",
+			"ability": "Blaze",
+			"item": "Charizardite Y",
+			"moves": [
+				"Heat Wave",
+				"Solar Beam",
+				"Overheat",
+				"Hidden Power Ground"
+			],
+			},
+		"Dragon Dance (Mega X)": {
+			"evs": {
+				"hp": 4,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Adamant",
+			"ability": "Blaze",
+			"item": "Charizardite X",
+			"moves": [
+				"Flare Blitz",
+				"Dragon Claw",
+				"Dragon Dance",
+				"Protect"
+			],
+			}
+			},
+	"Cresselia": {
+		"Offensive Trick Room": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 12,
+				"sa": 228,
+				"sd": 16,
+				"sp": 0
+			},
+			"ivs": {
+				"sp": 3
+			},
+			"nature": "Sassy",
+			"ability": "Levitate",
+			"item": "Expert Belt",
+			"moves": [
+				"Psychic",
+				"Ice Beam",
+				"Hidden Power Ground",
+				"Trick Room"
+			],
+			},
+		"Calm Mind": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 252,
+				"sa": 0,
+				"sd": 4,
+				"sp": 0
+			},
+			"nature": "Bold",
+			"ability": "Levitate",
+			"item": "Safety Goggles",
+			"moves": [
+				"Calm Mind",
+				"Psyshock",
+				"Moonlight",
+				"Trick Room"
+			],
+			},
+		"Support": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 100,
+				"sa": 44,
+				"sd": 112,
+				"sp": 0
+			},
+			"nature": "Calm",
+			"ability": "Levitate",
+			"item": "Sitrus Berry",
+			"moves": [
+				"Psyshock",
+				"Icy Wind",
+				"Helping Hand",
+				"Light Screen"
+			],
+			}
+			},
+	"Deoxys-A": {
+		"Fast Mixed Attacker": {
+			"evs": {
+				"hp": 0,
+				"at": 4,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Naive",
+			"ability": "Pressure",
+			"item": "Focus Sash",
+			"moves": [
+				"Psycho Boost",
+				"Superpower",
+				"Extreme Speed",
+				"Knock Off"
+			],
+			}
+		},
+	"Diancie": {
+		"Mega Mixed Attacker": {
+			"evs": {
+				"hp": 0,
+				"at": 208,
+				"df": 0,
+				"sa": 48,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Hasty",
+			"ability": "Clear Body",
+			"item": "Diancite",
+			"moves": [
+				"Diamond Storm",
+				"Moonblast",
+				"Earth Power",
+				"Protect"
+			],
+			},
+		"Offensive Trick Room": {
+			"evs": {
+				"hp": 252,
+				"at": 252,
+				"df": 0,
+				"sa": 0,
+				"sd": 4,
+				"sp": 0
+			},
+			"nature": "Brave",
+			"ability": "Clear Body",
+			"item": "Hard Stone",
+			"moves": [
+				"Diamond Storm",
+				"Moonblast",
+				"Trick Room",
+				"Protect"
+			],
+			}
+			},
+	"Ferrothorn": {
+		"Bulky Attacker": {
+			"evs": {
+				"hp": 252,
+				"at": 176,
+				"df": 80,
+				"sa": 0,
+				"sd": 0,
+				"sp": 0
+			},
+					"ivs": {
+				"sp": 0
+			},
+			"nature": "Brave",
+			"ability": "Iron Barbs",
+			"item": "Leftovers",
+			"moves": [
+				"Power Whip",
+				"Gyro Ball",
+				"Leech Seed",
+				"Protect"
+			],
+			}
+		},
 	"Gengar": {
 		"Bulky Attacker": {
 			"evs": {
@@ -126,6 +423,219 @@ var SETDEX_GEN6_SETS = {
 			],
 			}
 		},
+	"Gardevoir": {
+		"Trick Room Sweeper": {
+			"evs": {
+				"hp": 228,
+				"at": 0,
+				"df": 136,
+				"sa": 144,
+				"sd": 0,
+				"sp": 0
+			},
+			"nature": "Quiet",
+			"ability": "Trace",
+			"item": "Gardevoirite",
+			"moves": [
+				"Hyper Voice",
+				"Psychic",
+				"Psyshock",
+				"Trick Room"
+			],
+			},
+		"Special Sweeper": {
+			"evs": {
+				"hp": 8,
+				"at": 0,
+				"df": 0,
+				"sa": 248,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Timid",
+			"ability": "Trace",
+			"item": "Gardevoirite",
+			"moves": [
+				"Hyper Voice",
+				"Psychic",
+				"Focus Blast",
+				"Hidden Power Ground"
+			],
+			}
+		},
+	"Gastrodon": {
+		"Recover + 3 Attacks": {
+			"evs": {
+				"hp": 232,
+				"at": 0,
+				"df": 176,
+				"sa": 48,
+				"sd": 52,
+				"sp": 0
+			},
+			"ivs": {
+				"sp": 0
+			},
+			"nature": "Quiet",
+			"ability": "Storm Drain",
+			"item": "Sitrus Berry",
+			"moves": [
+				"Scald",
+				"Earth Power",
+				"Ice Beam",
+				"Recover"
+			],
+			}
+		},
+	"Genesect": {
+		"Choice Scarf": {
+			"evs": {
+				"hp": 0,
+				"at": 252,
+				"df": 0,
+				"sa": 64,
+				"sd": 0,
+				"sp": 192
+			},
+			"nature": "Naive",
+			"ability": "Download",
+			"item": "Choice Scarf",
+			"moves": [
+				"U-turn",
+				"Iron Head",
+				"Ice Beam",
+				"Explosion"
+			],
+			}
+		},
+	"Heatran": {
+		"Substitute": {
+			"evs": {
+				"hp": 24,
+				"at": 0,
+				"df": 0,
+				"sa": 232,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Modest",
+			"ability": "Flash Fire",
+			"item": "Leftovers",
+			"moves": [
+				"Heat Wave",
+				"Earth Power",
+				"Substitute",
+				"Protect"
+			],
+			}
+		},
+	"Hoopa-Unbound": {
+		"Mixed Attacker": {
+			"evs": {
+				"hp": 0,
+				"at": 192,
+				"df": 0,
+				"sa": 140,
+				"sd": 0,
+				"sp": 176
+			},
+			"nature": "Hasty",
+			"ability": "Magician",
+			"item": "Life Orb",
+			"moves": [
+				"Hyperspace Fury",
+				"Hyperspace Hole",
+				"Gunk Shot",
+				"Drain Punch"
+			],
+			},
+		"Offensive Trick Room": {
+			"evs": {
+				"hp": 0,
+				"at": 252,
+				"df": 44,
+				"sa": 208,
+				"sd": 4,
+				"sp": 0
+			},
+			"ivs": {
+				"sp": 0
+			},
+			"nature": "Brave",
+			"ability": "Magician",
+			"item": "Safety Goggles",
+			"moves": [
+				"Hyperspace Fury",
+				"Hyperspace Hole",
+				"Trick Room",
+				"Protect"
+			],
+			}
+		},
+	"Hydreigon": {
+		"Special Attacker": {
+			"evs": {
+				"hp": 0,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 4,
+				"sp": 252
+			},
+			"nature": "Modest",
+			"ability": "Levitate",
+			"item": "Life Orb",
+			"moves": [
+				"Draco Meteor",
+				"Dark Pulse",
+				"Fire Blast",
+				"Earth Power"
+			],
+			},
+		"Bulky Attacker": {
+			"evs": {
+				"hp": 216,
+				"at": 0,
+				"df": 0,
+				"sa": 152,
+				"sd": 16,
+				"sp": 124
+			},
+			"nature": "Modest",
+			"ability": "Levitate",
+			"item": "Life Orb",
+			"moves": [
+				"Draco Meteor",
+				"Dark Pulse",
+				"Roost",
+				"Protect"
+			],
+			}
+		},
+	"Jellicent": {
+		"Bulky Trick Room Setter": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 204,
+				"sa": 0,
+				"sd": 52,
+				"sp": 0
+			},
+			"ivs": {
+				"sp": 0
+			},
+			"nature": "Relaxed",
+			"ability": "Water Absorb",
+			"item": "Safety Goggles",
+			"moves": [
+				"Hydro Pump",
+				"Scald",
+				"Will-O-Wisp",
+				"Trick Room"
+			],
+			}
+		},
 	"Kangaskhan": {
 		"Memoric Bulky THALKS": {
 			"evs": {
@@ -182,6 +692,48 @@ var SETDEX_GEN6_SETS = {
 				"Double-Egde",
 				"Sucker Punch",
 				"Low Kick"
+			],
+			}
+		},
+	"Keldeo": {
+		"All-Out Attacker": {
+			"evs": {
+				"hp": 4,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Timid",
+			"ability": "Justify",
+			"item": "Life Orb",
+			"moves": [
+				"Hydro Pump",
+				"Secret Sword",
+				"Icy Wind",
+				"Protect"
+			],
+			}
+		},
+	"Kingdra": {
+		"Rain Attacker": {
+			"evs": {
+				"hp": 4,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Modest",
+			"ability": "Swift Swim",
+			"item": "Life Orb",
+			"moves": [
+				"Muddy Water",
+				"Draco Meteor",
+				"Hydro Pump",
+				"Protect"
 			],
 			}
 		},
@@ -319,6 +871,353 @@ var SETDEX_GEN6_SETS = {
 				"Rock Slide",
 				"Superpower",
 				"U-turn"
+			],
+			}
+		},
+	"Ludicolo": {
+		"Rain Sample": {
+			"evs": {
+				"hp": 4,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Modest",
+			"ability": "Swift Swim",
+			"item": "Rocky Helmet",
+			"moves": [
+				"Fake Out",
+				"Hydro Pump",
+				"Energy Ball",
+				"Ice Beam"
+			],
+			},
+		"Assault Vest": {
+			"evs": {
+				"hp": 104,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 152
+			},
+			"nature": "Modest",
+			"ability": "Swift Swim",
+			"item": "Assault Vest",
+			"moves": [
+				"Fake Out",
+				"Hydro Pump",
+				"Giga Drain",
+				"Ice Beam"
+			],
+			},
+		"Life Orb Attacker": {
+			"evs": {
+				"hp": 0,
+				"at": 0,
+				"df": 4,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Modest",
+			"ability": "Swift Swim",
+			"item": "Life Orb",
+			"moves": [
+				"Hydro Pump",
+				"Giga Drain",
+				"Ice Beam",
+				"Protect"
+			],
+			}
+		},
+	"Manectric": {
+		"Fast Attacker": {
+			"evs": {
+				"hp": 0,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 4,
+				"sp": 152
+			},
+			"nature": "Timid",
+			"ability": "Lightning Rod",
+			"item": "Manectite",
+			"moves": [
+				"Thunderbolt",
+				"Overheat",
+				"Hidden Power Ice",
+				"Protect"
+			],
+			}
+		},
+	"Politoed": {
+		"Rain Setter": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 252,
+				"sa": 0,
+				"sd": 4,
+				"sp": 0
+			},
+			"nature": "Bold",
+			"ability": "Drizzle",
+			"item": "Eject Button",
+			"moves": [
+				"Scald",
+				"Icy Wind",
+				"Helping Hand",
+				"Protect"
+			],
+			}
+		},
+	"Porygon2": {
+			"qsns Sample": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 160,
+				"sa": 44,
+				"sd": 52,
+				"sp": 0
+			},
+			"nature": "Modest",
+			"ability": "Download",
+			"item": "Eviolite",
+			"moves": [
+				"Ice Beam",
+				"Thunderbolt",
+				"Trick Room",
+				"Recover"
+			],
+			},
+		"Trick Room": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 160,
+				"sa": 20,
+				"sd": 76,
+				"sp": 0
+			},
+			"nature": "Sassy",
+			"ability": "Download",
+			"item": "Eviolite",
+			"moves": [
+				"Ice Beam",
+				"Foul Play",
+				"Trick Room",
+				"Recover"
+			],
+			}
+		},
+	"Scrafty": {
+		"Utility": {
+			"evs": {
+				"hp": 252,
+				"at": 212,
+				"df": 44,
+				"sa": 0,
+				"sd": 0,
+				"sp": 0
+			},
+			"ivs": {
+				"sp": 0
+			},
+			"nature": "Relaxed",
+			"ability": "Intimidate",
+			"item": "Sitrus Berry",
+			"moves": [
+				"Fake Out",
+				"Drain Punch",
+				"Knock Off",
+				"Quick Guard"
+			],
+			}
+		},
+	"Raichu": {
+		"Fast Support": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 0,
+				"sa": 4,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Timid",
+			"ability": "Lightning Rod",
+			"item": "Focus Sash",
+			"moves": [
+				"Fake Out",
+				"Volt Switch",
+				"Thunderbolt",
+				"Nuzzle"
+			],
+			}
+		},
+	"Rotom-Wash": {
+		"Special Tank": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 100,
+				"sa": 116,
+				"sd": 40,
+				"sp": 0
+			},
+			"nature": "Modest",
+			"ability": "Levitate",
+			"item": "Sitrus Berry",
+			"moves": [
+				"Hydro Pump",
+				"Thunderbolt",
+				"Will-O-Wisp",
+				"Thunder Wave"
+			],
+			},
+		"Choice Scarf": {
+			"evs": {
+				"hp": 4,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Timid",
+			"ability": "Levitate",
+			"item": "Choice Scarf",
+			"moves": [
+				"Hydro Pump",
+				"Thunderbolt",
+				"Volt Switch",
+				"Trick"
+			],
+			}
+		},
+	"Suicune": {
+		"Bulky Tailwind": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 16,
+				"sa": 144,
+				"sd": 40,
+				"sp": 56
+			},
+			"nature": "Bold",
+			"ability": "Pressure",
+			"item": "Sitrus Berry",
+			"moves": [
+				"Hydro Pump",
+				"Ice Beam",
+				"Snarl",
+				"Tailwind"
+			],
+		},
+	"Shaymin-Sky": {
+		"Offensive": {
+			"evs": {
+				"hp": 4,
+				"at": 0,
+				"df": 0,
+				"sa": 252,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Timid",
+			"ability": "Serene Grace",
+			"item": "Focus Sash",
+			"moves": [
+				"Seed Flare",
+				"Air Slash",
+				"Earth Power",
+				"Tailwind"
+			],
+			}
+		},
+	"Swampert": {
+		"Swift Swim Attacker": {
+			"evs": {
+				"hp": 0,
+				"at": 252,
+				"df": 4,
+				"sa": 0,
+				"sd": 0,
+				"sp": 252
+			},
+			"nature": "Adamant",
+			"ability": "Damp",
+			"item": "Swampertite",
+			"moves": [
+				"Waterfall",
+				"Earthquake",
+				"Ice Punch",
+				"Superpower"
+			],
+			},
+		"Non-Mega Support": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 44,
+				"sa": 156,
+				"sd": 56,
+				"sp": 0
+			},
+			"nature": "Quiet",
+			"ability": "Damp",
+			"item": "Sitrus Berry",
+			"moves": [
+				"Scald",
+				"Earth Power",
+				"Ice Beam",
+				"Icy Wind"
+			],
+			}
+		},
+	"Sylveon": {
+		"Choice Specs": {
+			"evs": {
+				"hp": 56,
+				"at": 0,
+				"df": 200,
+				"sa": 252,
+				"sd": 0,
+				"sp": 0
+			},
+			"nature": "Modest",
+			"ability": "Pixilate",
+			"item": "Choice Specs",
+			"moves": [
+				"Hyper Voice",
+				"Psyshock",
+				"Hidden Power Ground",
+				"Hyper Beam"
+			],
+			},
+		"Calm Mind": {
+			"evs": {
+				"hp": 248,
+				"at": 0,
+				"df": 164,
+				"sa": 88,
+				"sd": 8,
+				"sp": 0
+			},
+			"nature": "Modest",
+			"ability": "Pixilate",
+			"item": "Leftovers",
+			"moves": [
+				"Hyper Voice",
+				"Hidden Power Ground",
+				"Calm Mind",
+				"Substitute"
 			],
 			}
 		},
@@ -519,5 +1418,25 @@ var SETDEX_GEN6_SETS = {
 				"Protect"
 			],
 			}
+		},
+	"Zapdos": {
+		"Bulky Tailwind": {
+			"evs": {
+				"hp": 252,
+				"at": 0,
+				"df": 36,
+				"sa": 80,
+				"sd": 124,
+				"sp": 16
+			},
+			"nature": "Modest",
+			"ability": "Static",
+			"item": "Rocky Helmet",
+			"moves": [
+				"Thunderbolt",
+				"Hidden Power Ice",
+				"Tailwind",
+				"Roost"
+			],
 		}
 };
